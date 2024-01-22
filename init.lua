9f89a6881a99f73e1e5e051182d8d483cb6d42c7
john <jrobdikeman@gmail.com>
2024-01-21 23:05:10 -0600

    been a while since i rmeember to comit this :)

--- init.lua
+++ init.lua
@@ -2,6 +2,7 @@ vim.opt.relativenumber = true
 
 vim.opt.tabstop = 2
 vim.opt.shiftwidth = 2
+vim.opt.clipboard = 'unnamed' 
 
 vim.keymap.set('i', 'jk', '<Esc>')
 
@@ -21,13 +22,29 @@ Plug 'hrsh7th/nvim-cmp' -- autocomplete
 Plug 'hrsh7th/cmp-vsnip'
 Plug 'hrsh7th/vim-vsnip'
 Plug 'nvim-treesitter/nvim-treesitter'
+Plug 'jose-elias-alvarez/null-ls.nvim' -- some lsp thing for prettier plugin to work
+Plug 'MunifTanjim/prettier.nvim'
+Plug 'nvim-lua/plenary.nvim'
+
+Plug('nvim-telescope/telescope.nvim',{['tag'] = '0.1.5'})
 
 vim.fn["plug#end"]()
 
+vim.g.mapleader = " "
 vim.cmd("colorscheme gruvbox-material")
 
+
+-- telescope shit!!!
+local builtin = require('telescope.builtin')
+vim.keymap.set('n', '<leader>ff', builtin.find_files, {})
+vim.keymap.set('n', '<leader>fg', builtin.live_grep, {})
+vim.keymap.set('n', '<leader>fb', builtin.buffers, {})
+vim.keymap.set('n', '<leader>fh', builtin.help_tags, {})
+
+-- cute fterm 
 vim.keymap.set({'n','t'}, '<Leader>i', '<CMD>lua require("FTerm").toggle()<CR>')
 
+-- how  to make p, y, x, to copy-paste from system clipboard by default  
 local capabilities = require("cmp_nvim_lsp").default_capabilities()
 
 -- Setup language servers.
@@ -126,4 +143,78 @@ vim.api.nvim_create_autocmd('LspAttach', {
       vim.lsp.buf.format { async = true }
     end, opts)
   end,
+})
+
+ -- null-ls, the LSP thing for the prettier plugin
+local null_ls = require("null-ls")
+
+local group = vim.api.nvim_create_augroup("lsp_format_on_save", { clear = false })
+local event = "BufWritePre" -- or "BufWritePost"
+local async = event == "BufWritePost"
+
+null_ls.setup({
+  on_attach = function(client, bufnr)
+    if client.supports_method("textDocument/formatting") then
+      vim.keymap.set("n", "<Leader>f", function()
+        vim.lsp.buf.format({ bufnr = vim.api.nvim_get_current_buf() })
+      end, { buffer = bufnr, desc = "[lsp] format" })
+
+      -- format on save
+      vim.api.nvim_clear_autocmds({ buffer = bufnr, group = group })
+      vim.api.nvim_create_autocmd(event, {
+        buffer = bufnr,
+        group = group,
+        callback = function()
+          vim.lsp.buf.format({ bufnr = bufnr, async = async })
+        end,
+        desc = "[lsp] format on save",
+      })
+    end
+
+    if client.supports_method("textDocument/rangeFormatting") then
+      vim.keymap.set("x", "<Leader>f", function()
+        vim.lsp.buf.format({ bufnr = vim.api.nvim_get_current_buf() })
+      end, { buffer = bufnr, desc = "[lsp] format" })
+    end
+  end,
+})
+
+local prettier = require("prettier")
+
+prettier.setup({
+  bin = 'prettierd', -- or `'prettierd'` (v0.23.3+)
+  filetypes = {
+    "css",
+    "graphql",
+    "html",
+    "javascript",
+    "javascriptreact",
+    "json",
+    "less",
+    "markdown",
+    "scss",
+    "typescript",
+    "typescriptreact",
+    "yaml",
+  },
+  cli_options = {
+    arrow_parens = "always",
+    bracket_spacing = true,
+    bracket_same_line = false,
+    embedded_language_formatting = "auto",
+    end_of_line = "lf",
+    html_whitespace_sensitivity = "css",
+    -- jsx_bracket_same_line = false,
+    jsx_single_quote = false,
+    print_width = 80,
+    prose_wrap = "preserve",
+    quote_props = "as-needed",
+    semi = true,
+    single_attribute_per_line = false,
+    single_quote = false,
+    tab_width = 2,
+    trailing_comma = "es5",
+    use_tabs = false,
+    vue_indent_script_and_style = false,
+  },
 })
