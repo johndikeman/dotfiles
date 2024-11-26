882cf02819d6dd8103cd4aea47232736711ef35e
john <jrobdikeman@gmail.com>
2024-11-25 18:48:23 -0600

    run format oops. add workspace and telescope thing

--- init.lua
+++ init.lua
@@ -102,9 +102,9 @@ require("lazy").setup({
 		ft = "mchat",
 
 		keys = {
-			{ "<C-m>d",       ":Mdelete<cr>", mode = "n" },
-			{ "<C-m>s",       ":Mselect<cr>", mode = "n" },
-			{ "<C-m><space>", ":Mchat<cr>",   mode = "n" },
+			{ "<C-m>d", ":Mdelete<cr>", mode = "n" },
+			{ "<C-m>s", ":Mselect<cr>", mode = "n" },
+			{ "<C-m><space>", ":Mchat<cr>", mode = "n" },
 		},
 
 		config = function()
@@ -139,17 +139,21 @@ require("lazy").setup({
 		end,
 	},
 	{
-		'numToStr/Comment.nvim',
+		"numToStr/Comment.nvim",
 		opts = {
 			-- add any options here
-		}
+		},
 	},
-	{ 'JoosepAlviste/nvim-ts-context-commentstring' },
+	{ "JoosepAlviste/nvim-ts-context-commentstring" },
 	{
 		"chentoast/marks.nvim",
 		event = "VeryLazy",
 		opts = {},
-	}
+	},
+	{
+		"natecraddock/workspaces.nvim",
+		tag = "1.0",
+	},
 })
 
 -- gruvbox
@@ -197,7 +201,6 @@ mason_lspconfig.setup_handlers({
 		})
 	end,
 })
-
 
 -- cute fterm
 vim.keymap.set({ "n", "t" }, "<Leader>i", '<CMD>lua require("FTerm").toggle()<CR>')
@@ -254,7 +257,21 @@ cmp.setup({
 
 -- treesitter stuff
 require("nvim-treesitter.configs").setup({
-	ensure_installed = { "lua", "vim", "vimdoc", "query", "markdown", "markdown_inline", "python", "rust", "gdscript", "svelte", "typescript", "javascript", "css" },
+	ensure_installed = {
+		"lua",
+		"vim",
+		"vimdoc",
+		"query",
+		"markdown",
+		"markdown_inline",
+		"python",
+		"rust",
+		"gdscript",
+		"svelte",
+		"typescript",
+		"javascript",
+		"css",
+	},
 	highlight = {
 		enable = true,
 		-- Setting this to true will run `:h syntax` and tree-sitter at the same time.
@@ -272,6 +289,11 @@ vim.keymap.set("n", "<leader>fg", builtin.live_grep, {})
 vim.keymap.set("n", "<leader>fb", builtin.buffers, {})
 vim.keymap.set("n", "<leader>fh", builtin.help_tags, {})
 vim.keymap.set("n", "<leader>fs", builtin.treesitter, {})
+
+local telescope = require("telescope")
+telescope.load_extension("workspaces")
+vim.keymap.set("n", "<leader>fw", require("telescope").extensions.workspaces.workspaces, {})
+
 -- Global mappings for vim LSP
 -- See `:help vim.diagnostic.*` for documentation on any of the below functions
 vim.keymap.set("n", "<space>e", vim.diagnostic.open_float)
@@ -401,50 +423,72 @@ prettier.setup({
 	},
 })
 
+require("gitsigns").setup()
 
-require('gitsigns').setup()
-
-require('ts_context_commentstring').setup {
+require("ts_context_commentstring").setup({
 	enable_autocmd = false,
-}
+})
 
-require('Comment').setup {
-	pre_hook = require('ts_context_commentstring.integrations.comment_nvim').create_pre_hook(),
-}
+require("Comment").setup({
+	pre_hook = require("ts_context_commentstring.integrations.comment_nvim").create_pre_hook(),
+})
 
-require'marks'.setup {
-  -- whether to map keybinds or not. default true
-  default_mappings = true,
-  -- which builtin marks to show. default {}
-  builtin_marks = { ".", "<", ">", "^" },
-  -- whether movements cycle back to the beginning/end of buffer. default true
-  cyclic = true,
-  -- whether the shada file is updated after modifying uppercase marks. default false
-  force_write_shada = false,
-  -- how often (in ms) to redraw signs/recompute mark positions. 
-  -- higher values will have better performance but may cause visual lag, 
-  -- while lower values may cause performance penalties. default 150.
-  refresh_interval = 250,
-  -- sign priorities for each type of mark - builtin marks, uppercase marks, lowercase
-  -- marks, and bookmarks.
-  -- can be either a table with all/none of the keys, or a single number, in which case
-  -- the priority applies to all marks.
-  -- default 10.
-  sign_priority = { lower=10, upper=15, builtin=8, bookmark=20 },
-  -- disables mark tracking for specific filetypes. default {}
-  excluded_filetypes = {},
-  -- disables mark tracking for specific buftypes. default {}
-  excluded_buftypes = {},
-  -- marks.nvim allows you to configure up to 10 bookmark groups, each with its own
-  -- sign/virttext. Bookmarks can be used to group together positions and quickly move
-  -- across multiple buffers. default sign is '!@#$%^&*()' (from 0 to 9), and
-  -- default virt_text is "".
-  bookmark_0 = {
-    sign = "☭",
-    virt_text = "yo",
-    -- explicitly prompt for a virtual line annotation when setting a bookmark from this group.
-    -- defaults to false.
-    annotate = true,
-  },
-  mappings = {}
-}
+require("marks").setup({
+	-- whether to map keybinds or not. default true
+	default_mappings = true,
+	-- which builtin marks to show. default {}
+	builtin_marks = { ".", "<", ">", "^" },
+	-- whether movements cycle back to the beginning/end of buffer. default true
+	cyclic = true,
+	-- whether the shada file is updated after modifying uppercase marks. default false
+	force_write_shada = false,
+	-- how often (in ms) to redraw signs/recompute mark positions.
+	-- higher values will have better performance but may cause visual lag,
+	-- while lower values may cause performance penalties. default 150.
+	refresh_interval = 250,
+	-- sign priorities for each type of mark - builtin marks, uppercase marks, lowercase
+	-- marks, and bookmarks.
+	-- can be either a table with all/none of the keys, or a single number, in which case
+	-- the priority applies to all marks.
+	-- default 10.
+	sign_priority = { lower = 10, upper = 15, builtin = 8, bookmark = 20 },
+	-- disables mark tracking for specific filetypes. default {}
+	excluded_filetypes = {},
+	-- disables mark tracking for specific buftypes. default {}
+	excluded_buftypes = {},
+	-- marks.nvim allows you to configure up to 10 bookmark groups, each with its own
+	-- sign/virttext. Bookmarks can be used to group together positions and quickly move
+	-- across multiple buffers. default sign is '!@#$%^&*()' (from 0 to 9), and
+	-- default virt_text is "".
+	bookmark_0 = {
+		sign = "☭",
+		virt_text = "yo",
+		-- explicitly prompt for a virtual line annotation when setting a bookmark from this group.
+		-- defaults to false.
+		annotate = true,
+	},
+	mappings = {},
+})
+
+require("workspaces").setup({
+
+	auto_open = false,
+	hooks = {
+		add = {},
+		remove = {},
+		rename = {},
+		open_pre = {},
+		open = { "Telescope find_files" },
+	},
+})
+
+telescope.setup({
+	extensions = {
+		workspaces = {
+			-- keep insert mode after selection in the picker, default is false
+			keep_insert = true,
+			-- Highlight group used for the path in the picker, default is "String"
+			path_hl = "String",
+		},
+	},
+})
