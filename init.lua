0191462952baaede3432480ec24426d92053572b
john <jrobdikeman@gmail.com>
2024-12-08 10:45:15 -0600

    run format

--- init.lua
+++ init.lua
@@ -102,9 +102,9 @@ require("lazy").setup({
 		ft = "mchat",
 
 		keys = {
-			{ "<C-m>d", ":Mdelete<cr>", mode = "n" },
-			{ "<C-m>s", ":Mselect<cr>", mode = "n" },
-			{ "<C-m><space>", ":Mchat<cr>", mode = "n" },
+			{ "<C-m>d",       ":Mdelete<cr>", mode = "n" },
+			{ "<C-m>s",       ":Mselect<cr>", mode = "n" },
+			{ "<C-m><space>", ":Mchat<cr>",   mode = "n" },
 		},
 
 		config = function()
