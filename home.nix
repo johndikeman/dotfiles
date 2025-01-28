8c79a8d6d95d3a6e4fd359482e933d4397a48a2f
john <jrobdikeman@gmail.com>
2025-01-28 10:39:08 -0600

    add xclip for nvim system clipboard usage

--- home.nix
+++ home.nix
@@ -34,6 +34,7 @@
 		pkgs.glibc
 		pkgs.gcc
 		pkgs.gcc.cc.lib
+		pkgs.xclip
     # # It is sometimes useful to fine-tune packages, for example, by applying
     # # overrides. You can do that directly here, just don't forget the
     # # parentheses. Maybe you want to install Nerd Fonts with a limited number of
