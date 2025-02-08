7fa410abe50fadc8e3ccbf03eac875b0a1bc32e6
john <jrobdikeman@gmail.com>
2025-02-07 22:28:34 -0600

    add nix formatter

--- home.nix
+++ home.nix
@@ -303,6 +303,7 @@ in
 		pkgs.maturin
 		pkgs.niv
 		pkgs.ncdu
+		pkgs.nixfmt-rfc-style
     # # It is sometimes useful to fine-tune packages, for example, by applying
     # # overrides. You can do that directly here, just don't forget the
     # # parentheses. Maybe you want to install Nerd Fonts with a limited number of
