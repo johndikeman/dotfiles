fc031a483163d3cd678a5125ab5aaedc37c90ea1
john <jrobdikeman@gmail.com>
2025-02-23 08:58:18 -0600

    try adding some playwright dependencies

--- home.nix
+++ home.nix
@@ -324,6 +324,9 @@ in
     pkgs.nixfmt-rfc-style
     pkgs.obsidian
 		pkgs.tmux
+		pkgs.google-cloud-sdk
+		pkgs.libevent # dependencies for playwright for some reason
+		pkgs.flite
     # # It is sometimes useful to fine-tune packages, for example, by applying
     # # overrides. You can do that directly here, just don't forget the
     # # parentheses. Maybe you want to install Nerd Fonts with a limited number of
