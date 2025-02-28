1b420a17fc10b9fc7b6d32893c170ffd687e930a
john <jrobdikeman@gmail.com>
2025-02-28 07:44:32 -0600

    mullvad

--- home.nix
+++ home.nix
@@ -327,6 +327,7 @@ in
 		pkgs.google-cloud-sdk
 		pkgs.libevent # dependencies for playwright for some reason
 		pkgs.flite
+		pkgs.mullvad-vpn
     # # It is sometimes useful to fine-tune packages, for example, by applying
     # # overrides. You can do that directly here, just don't forget the
     # # parentheses. Maybe you want to install Nerd Fonts with a limited number of
