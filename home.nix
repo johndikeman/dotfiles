22af040a8b94796307683fc0e4a6b4d91a2b2ac6
john <jrobdikeman@gmail.com>
2025-03-09 14:23:17 -0500

    add uv package manager

--- home.nix
+++ home.nix
@@ -328,6 +328,7 @@ in
 		pkgs.libevent # dependencies for playwright for some reason
 		pkgs.flite
 		pkgs.mullvad-vpn
+		pkgs.uv
     # # It is sometimes useful to fine-tune packages, for example, by applying
     # # overrides. You can do that directly here, just don't forget the
     # # parentheses. Maybe you want to install Nerd Fonts with a limited number of
