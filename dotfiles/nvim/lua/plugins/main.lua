164ab342191c8a2005586892b297985cafe31ac2
john <jrobdikeman@gmail.com>
2025-05-12 18:12:54 -0500

    add typescript formatting

--- dotfiles/nvim/lua/plugins/main.lua
+++ dotfiles/nvim/lua/plugins/main.lua
@@ -320,6 +320,7 @@ return {
 				lua = { "stylua" },
 				python = { "black" },
 				javascript = { "prettierd", "prettier", stop_after_first = true },
+				typescript = { "prettierd", "prettier", stop_after_first = true },
 				svelte = { "prettierd", "prettier", stop_after_first = true },
 				nix = { "nixfmt" },
 			},
