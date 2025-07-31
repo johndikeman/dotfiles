2bb5d216c6c1726a16d4c83948527bf3adccec9c
john <jrobdikeman@gmail.com>
2025-07-31 15:58:02 -0500

    add copied swww script

--- nix/hyprland.nix
+++ nix/hyprland.nix
@@ -31,6 +31,37 @@
     papirus-icon-theme # Additional icon theme
     catppuccin-gtk # GTK theme
     # Fonts are managed in configuration.nix
+
+    (pkgs.writeShellApplication {
+      name = "swww-randomize.sh";
+      runtimeInputs = [ swww ];
+      text = ''
+        DEFAULT_INTERVAL=300 # In seconds
+
+        if [ $# -lt 1 ] || [ ! -d "$1" ]; then
+        	printf "Usage:\n\t\e[1m%s\e[0m \e[4mDIRECTORY\e[0m [\e[4mINTERVAL\e[0m]\n" "$0"
+        	printf "\tChanges the wallpaper to a randomly chosen image in DIRECTORY every\n\tINTERVAL seconds (or every %d seconds if unspecified)." "$DEFAULT_INTERVAL"
+        	exit 1
+        fi
+
+        # See swww-img(1)
+        RESIZE_TYPE="crop"
+        export SWWW_TRANSITION_FPS="${SWWW_TRANSITION_FPS: -60}"
+        export SWWW_TRANSITION_STEP="${SWWW_TRANSITION_STEP: -2}"
+
+        while true; do
+        	find "$1" -type f \
+        	| while read -r img; do
+        		echo "$(</dev/urandom tr -dc a-zA-Z0-9 | head -c 8):$img"
+        	done \
+        	| sort -n | cut -d':' -f2- \
+        	| while read -r img; do
+        		swww img --resize="$RESIZE_TYPE" -t random "$img"
+        		sleep "${"2:-$DEFAULT_INTERVAL"}"
+        	done
+        done
+        		'';
+    })
   ];
 
   # Enable Hyprland
@@ -48,7 +79,6 @@
         "nm-applet"
         "blueman-applet"
         "/usr/libexec/polkit-gnome-authentication-agent-1"
-        "swww init"
       ];
 
       monitor = [
