77a99c3db3e7dde423edc90df9019e77ba8334d0
john <jrobdikeman@gmail.com>
2025-10-01 16:29:27 -0500

    add idle

--- nix/hyprland.nix
+++ nix/hyprland.nix
@@ -45,6 +45,60 @@
     })
   ];
 
+  services.swayidle =
+    let
+      # Lock command
+      lock = "${pkgs.swaylock}/bin/swaylock --daemonize";
+      # TODO: modify "display" function based on your window manager
+      # Sway
+      display = status: "swaymsg 'output * power ${status}'";
+      # Hyprland
+      # display = status: "hyprctl dispatch dpms ${status}";
+      # Niri
+      # display = status: "${pkgs.niri}/bin/niri msg action power-${status}-monitors";
+    in
+    {
+      enable = true;
+      timeouts = [
+        {
+          timeout = 280; # in seconds
+          command = "${pkgs.libnotify}/bin/notify-send 'Locking in 5 seconds' -t 5000";
+        }
+        {
+          timeout = 300;
+          command = lock;
+        }
+        {
+          timeout = 420;
+          command = display "off";
+          resumeCommand = display "on";
+        }
+        {
+          timeout = 600;
+          command = "${pkgs.systemd}/bin/systemctl suspend";
+        }
+      ];
+      events = [
+        {
+          event = "before-sleep";
+          # adding duplicated entries for the same event may not work
+          command = (display "off") + "; " + lock;
+        }
+        {
+          event = "after-resume";
+          command = display "on";
+        }
+        {
+          event = "lock";
+          command = (display "off") + "; " + lock;
+        }
+        {
+          event = "unlock";
+          command = display "on";
+        }
+      ];
+    };
+
   # Enable Hyprland
   wayland.windowManager.hyprland = {
     enable = true;
