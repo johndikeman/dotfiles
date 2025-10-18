b15a72164f3ad420818aaaa1f29f0575ec020a47
john <jrobdikeman@gmail.com>
2025-10-18 15:45:22 -0500

    add powermenu to style rules and simplify script slightly

--- scripts/powermenu.sh
+++ scripts/powermenu.sh
@@ -3,17 +3,14 @@ COMMANDS=('loginctl lock-session' 'hyprctl dispatch exit' 'systemctl poweroff' '
 
 MENU_ITEMS=()
 for i in "${!DISPLAY_NAMES[@]}"; do
-  MENU_ITEMS+=("text:${DISPLAY_NAMES[i]}")
+  MENU_ITEMS+=("${DISPLAY_NAMES[i]}")
 done
 
 CHOICE=$(printf '%s\n' "${MENU_ITEMS[@]}" | wofi --show dmenu --prompt "Choose an action")
 
-# Extract label from `text:...`
-SELECTED_NAME="${CHOICE#*:text:}"
-
 # Match selection and run command
 for i in "${!DISPLAY_NAMES[@]}"; do
-  if [[ "${DISPLAY_NAMES[i]}" == "$SELECTED_NAME" ]]; then
+  if [[ "${DISPLAY_NAMES[i]}" == "$CHOICE" ]]; then
     eval "${COMMANDS[i]}"
     break
   fi
