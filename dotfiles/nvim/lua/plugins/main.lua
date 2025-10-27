c148e08d869b0698c1cfef49853e5a52fd8ad85c
john <jrobdikeman@gmail.com>
2025-10-27 08:33:31 -0500

    Revert "switch to new vim lsp syntax"
    This reverts commit 8ac31b9d8a96f5437bc6f1247c90484622324ae8.

--- dotfiles/nvim/lua/plugins/main.lua
+++ dotfiles/nvim/lua/plugins/main.lua
@@ -23,12 +23,12 @@ return {
 			},
 		},
 		config = function(_, opts)
+			local lspconfig = require("lspconfig")
 			for server, config in pairs(opts.servers) do
 				-- passing config.capabilities to blink.cmp merges with the capabilities in your
 				-- `opts[server].capabilities, if you've defined it
 				config.capabilities = require("blink.cmp").get_lsp_capabilities(config.capabilities)
-				-- new style config
-				vim.lsp.config(server, config)
+				lspconfig[server].setup(config)
 			end
 		end,
 	},
