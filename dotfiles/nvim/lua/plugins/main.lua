68646d31d805af59f19611f943233f57d11f716e
john <jrobdikeman@gmail.com>
2025-12-09 14:49:18 -0600

    attempt to fix lsp

--- dotfiles/nvim/lua/plugins/main.lua
+++ dotfiles/nvim/lua/plugins/main.lua
@@ -23,12 +23,13 @@ return {
 			},
 		},
 		config = function(_, opts)
-			local lspconfig = require("lspconfig")
+			local bcmp = require("blink.cmp")
 			for server, config in pairs(opts.servers) do
 				-- passing config.capabilities to blink.cmp merges with the capabilities in your
 				-- `opts[server].capabilities, if you've defined it
-				config.capabilities = require("blink.cmp").get_lsp_capabilities(config.capabilities)
-				lspconfig[server].setup(config)
+				config.capabilities = bcmp.get_lsp_capabilities(config.capabilities)
+				vim.lsp.config(server, config)
+				vim.lsp.enable(server)
 			end
 		end,
 	},
