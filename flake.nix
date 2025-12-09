fb5c1a57013bca58e66d808bd6c83cde5ba0edf0
john <jrobdikeman@gmail.com>
2025-12-09 17:01:33 -0600

    install nix-colors and theme kitty with it

--- flake.nix
+++ flake.nix
@@ -30,6 +30,7 @@
       # Hyprspace uses latest Hyprland. We declare this to keep them in sync.
       inputs.hyprland.follows = "hyprland";
     };
+    nix-colors.url = "github:misterio77/nix-colors";
   };
 
   outputs =
