43b64530e157ea75442c554ea529aa9b928a821e
john <jrobdikeman@gmail.com>
2025-12-12 11:46:18 -0600

    prettier-plugin-svelte use for format

--- dotfiles/nvim/lua/plugins/main.lua
+++ dotfiles/nvim/lua/plugins/main.lua
@@ -286,7 +286,7 @@ return {
 				python = { "black" },
 				javascript = { "prettierd", "prettier", stop_after_first = true },
 				typescript = { "prettierd", "prettier", stop_after_first = true },
-				svelte = { "prettierd", "prettier", stop_after_first = true },
+				svelte = { "prettier-plugin-svelte" },
 				nix = { "nixfmt" },
 			},
 			-- Set default options
