28540dd8dd4b5a943889f9e930b1afb2166be665
john <jrobdikeman@gmail.com>
2026-03-24 13:43:20 -0500

    change wofi colors

--- nix/hyprland.nix
+++ nix/hyprland.nix
@@ -432,7 +432,7 @@
     };
     style = pkgs.replaceVars ../css/wofi.css (
       lib.attrsets.filterAttrs (
-        name: value: lib.strings.hasInfix name "base01 base02 base03 base05"
+        name: value: lib.strings.hasInfix name "base00 base01 base02 base03 base04 base05 base06 base07 base08 base09 base0A base0B base0C base0D base0E base0F"
       ) config.colorScheme.palette
     );
   };
