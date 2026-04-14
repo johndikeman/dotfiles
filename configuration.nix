44259d0568ecee9030a05aa2cf06ef63bc297d3f
john <jrobdikeman@gmail.com>
2026-04-14 13:28:44 -0500

    Revert "adding llama.cpp and hf fetch"
    This reverts commit 33a04f88424696b3320cf57df4ed66b47c81823d.

--- configuration.nix
+++ configuration.nix
@@ -321,8 +321,6 @@
   # List packages installed in system profile. To search, run:
   # $ nix search wget
   environment.systemPackages = [
-    inputs.llama-cpp.packages.${pkgs.system}.cuda
-    pkgs.python313Packages.huggingface-hub
     pkgs.neovim # Do not forget to add an editor to edit configuration.nix! The Nano editor is also installed by default.
     pkgs.git
     pkgs.unzip
--- flake.nix
+++ flake.nix
@@ -25,9 +25,6 @@
       inputs.hyprlang.follows = "hyprland/hyprlang";
     };
     nix-colors.url = "github:misterio77/nix-colors";
-    llama-cpp = {
-      url = "github:ggml-org/llama.cpp";
-    };
   };
 
   outputs =
@@ -37,7 +34,6 @@
       home-manager,
       lanzaboote,
       rose-pine-hyprcursor,
-      llama-cpp,
       ...
     }@inputs:
     {
