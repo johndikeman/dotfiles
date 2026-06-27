d0cdf09052cb309473d430458dd45224d599fdbe
john <jrobdikeman@gmail.com>
2026-06-26 21:35:49 -0500

    add flameshot

--- home.nix
+++ home.nix
@@ -144,6 +144,25 @@
     # '';
   };
 
+  services.flameshot = {
+    enable = true;
+    settings = {
+      General = {
+        showStartupLaunchMessage = false;
+        # Auto save to this path
+        savePath = "${config.home.homeDirectory}/Pictures/screenshots";
+        savePathFixed = true;
+        saveAsFileExtension = ".jpg";
+        filenamePattern = "%F_%H-%M";
+        drawThickness = 1;
+        copyPathAfterSave = true;
+
+        # For wayland
+        useGrimAdapter = true;
+      };
+    };
+  };
+
   programs.tmux = {
     enable = true;
     shell = "/home/dikeman/.nix-profile/bin/fish";
