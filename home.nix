8b3081ea391537df3a6475bd46a54dc6139409b0
john <jrobdikeman@gmail.com>
2026-06-26 22:13:42 -0500

    conf stuff

--- home.nix
+++ home.nix
@@ -115,6 +115,7 @@
     # # symlink to the Nix store copy.
     # ".screenrc".source = dotfiles/screenrc;
 
+    ".config/pipe-viewer/pipe-viewer.conf".source = dotfiles/pipe-viewer/pipe-viewer.conf;
     ".config/yt-channels.txt".source = ./yt-channels.txt;
 
     "${config.xdg.configHome}/containers" = {
