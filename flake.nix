471b26e1fbef399d71ca8db624988f7c5e248164
john <jrobdikeman@gmail.com>
2026-08-28 15:06:24 -0500

    Add mcserver flake module (Minecraft server)

--- flake.nix
+++ flake.nix
@@ -31,6 +31,8 @@
       inputs.nixpkgs.follows = "nixpkgs";
     };
     wallpapers.url = "github:johndikeman/wallpapers";
+    # Local flake containing the mcserver NixOS module (Minecraft server)
+    mcserver.url = "path:/home/john/experiments";
   };
 
   outputs =
@@ -41,6 +43,7 @@
       lanzaboote,
       rose-pine-hyprcursor,
       helium,
+      mcserver,
       ...
     }@inputs:
     {
@@ -54,6 +57,12 @@
         };
         modules = [
           lanzaboote.nixosModules.lanzaboote
+          # Modded Minecraft server (All the Mods 10: Aeronautics)
+          {
+            imports = [ mcserver.nixosModules.mcserver ];
+            services.mcserver.enable = true;
+            services.mcserver.dataDir = "/home/john/experiments/serverfiles";
+          }
           # Import the previous configuration.nix we used,
           # so the old configuration file still takes effect
           ./configuration.nix
