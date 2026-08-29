d7b80886fc42a01e9ccbb28638e054e18273f631
john <jrobdikeman@gmail.com>
2026-08-29 11:43:46 -0500

    zip

--- home.nix
+++ home.nix
@@ -33,6 +33,7 @@
     # # Adds the 'hello' command to your environment. It prints a friendly
     # # "Hello, world!" when run.
     # pkgs.hello
+    pkgs.zip
     pkgs.prismlauncher
     pkgs.gh
     pkgs.nodejs_22
